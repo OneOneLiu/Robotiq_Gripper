e50b8ffe6280248b9781d82e8cf32ec663f1e7ce
OneOneLiu <daohui.liu@mail.utoronto.ca>
2024-05-29 19:14:42 -0400

    update sim gripper control (remove slider styles)

--- src/sim_gripper_control.py
+++ src/sim_gripper_control.py
@@ -15,40 +15,25 @@ class GripperControl:
         self.publisher = rospy.Publisher('/gripper_controller/follow_joint_trajectory/goal', 
                                          FollowJointTrajectoryActionGoal, queue_size=10)
 
-        # Styling the slider
-        style = ttk.Style()
-        style.theme_use('clam')  # Use the 'clam' theme as the base for customization
-        style.configure("TScale", sliderlength=40, background="#333", foreground="#fff", troughcolor="#555", bordercolor="#666", lightcolor="#777", darkcolor="#444")
-
         # GUI setup
-        self.slider_value = tk.DoubleVar()
         ttk.Label(master, text="Gripper Position:").pack()
         self.slider = ttk.Scale(master, from_=0.0, to=0.8, orient='horizontal', 
-                                variable=self.slider_value, length=300,  # Make the slider longer
-                                command=self.update_value, style="TScale")
-        self.slider.pack(pady=20)  # Add some padding around the slider
+                                length=300,  # Make the slider longer
+                                command=self.control_gripper)
+        self.slider.pack(fill=tk.X, expand=True)  # Add some padding around the slider
 
-        # Button to send the command
-        self.send_button = ttk.Button(master, text="Send Command", command=self.send_command)
-        self.send_button.pack(pady=10)  # Add padding below the button
-
-    def update_value(self, event):
-        # This function is called whenever the slider is moved
-        value = self.slider_value.get()
-        rospy.loginfo("Slider updated to: %f", value)
-
-    def send_command(self):
+    def control_gripper(self, value):
         # Prepare the message
         msg = FollowJointTrajectoryActionGoal()
         msg.goal.trajectory.joint_names = ["robotiq_85_left_knuckle_joint"] 
         point = JointTrajectoryPoint()
-        point.positions = [self.slider_value.get()]
+        point.positions = [float(value)]
         point.time_from_start = rospy.Duration(1.0)
         msg.goal.trajectory.points.append(point)
         
         # Publish the message
         self.publisher.publish(msg)
-        rospy.loginfo("Sent gripper command: %f", self.slider_value.get())
+        rospy.loginfo("Sent gripper command: %f", float(value))
 
 def main():
     root = tk.Tk()
